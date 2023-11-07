7f0034d1ff2a55aa51638a82c976cd6fa640f8d0
sm-nat <141369048+sm-nat@users.noreply.github.com>
2023-11-07 00:10:22 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -13,6 +13,17 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable= False)
+    email = Column(String(250), nullable=False)
+
+class User(Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer, ForeignKey('users.id'))
+    username = Column(String(120), nullable=False, unique=True)
+    password = Column(String(120), nullable=False)
+    active = Column(Boolean(), default=True)
+    last_login = Column(DateTime())
+
 
 class Address(Base):
     __tablename__ = 'address'
